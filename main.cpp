54122e223e63f0b48e0e5f735913701d656ca28f
tonegas <tonegas@ProfiloOnirico.(none)>
2009-10-19 23:06:40 +0200

    INIZIO

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: tonegas
+ *
+ * Created on 19 ottobre 2009, 22.41
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
