4ff3fbfbf42b0eb3dff08ba2fa9aeff490c459ba
tonegas <tonegas@ProfiloOnirico.(none)>
2009-10-19 23:44:09 +0200

    un po' di struttura

--- main.cpp
+++ main.cpp
@@ -5,13 +5,38 @@
  * Created on 19 ottobre 2009, 22.41
  */
 
+#include <SDL/SDL.h>
 #include <stdlib.h>
+
+typedef enum {
+    MENU,
+    PARTITA,
+    EDITOR
+} Stato_t;
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-
+    SDL_Event *evento;
+    while (SDL_WaitEvent(evento)) {
+        switch (Stato_t) {
+            case MENU:
+                break;
+            case PARTITA:
+                statusPartita(evento);
+                break;
+            case EDITOR:
+                break;
+        }
+        free(evento);
+    }
     return (EXIT_SUCCESS);
 }
 
+void statusPartita(SDL_Event *evento) {
+    switch (evento->type) {
+        case SDL_KEYDOWN:
+            SDL_KeyboardEvent *ke = (SDL_KeyboardEvent *) evento;
+    }
+}
